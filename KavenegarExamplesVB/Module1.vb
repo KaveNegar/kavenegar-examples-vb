02a6987445da4b6764b9f166aec3c67f8ba9fd34
bgsrb88 <bgsrb88@gmail.com>
2016-10-19 12:43:26 +0330

    first commit

---
+++ KavenegarExamplesVB/Module1.vb
@@ -0,0 +1,30 @@
+﻿Imports Kavenegar
+Imports Kavenegar.Excepctions
+Module Module1
+ Sub Main()
+	VerifyLookup()
+ End Sub
+ Sub VerifyLookup()
+	Try
+	 Dim api As New KavenegarApi("")
+	 Const receptor = ""
+	 Const token = ""
+	 Const token2 = ""
+	 Const token3 = ""
+	 Const template = ""
+	 Dim message = api.VerifyLookup(receptor, token, token2, token3, template)
+	 Console.WriteLine("Messageid : " & message.Messageid)
+	 Console.WriteLine("Message : " & message.Message)
+	 Console.WriteLine("Receptor : " & message.Receptor)
+	 Console.WriteLine("Sender : " & message.Sender)
+	 Console.WriteLine("Date : " & message.Date)
+	 Console.ReadKey()
+	Catch ex As ApiException
+	 Console.WriteLine("Message : " & ex.Message)
+	 Console.ReadKey()
+	Catch ex As HttpException
+	 Console.WriteLine("Message : " & ex.Message)
+	 Console.ReadKey()
+	End Try
+ End Sub
+End Module
